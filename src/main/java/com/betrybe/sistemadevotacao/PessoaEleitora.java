29b120beb5ae507c8ea6e5dcbece10cd993ee7c6
Ana Lourenco <analuizajl5@gmail.com>
2024-04-10 18:24:38 -0300

    pessoa eleitora implemented

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,27 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * implementing class PessoaEleitora.
+ */
+public class PessoaEleitora extends Pessoa {
+  /**
+   * methods and attributes of PessoaEleitora.
+   */
+  private String cpf;
 
+  /**
+   * constructor PessoaEleitora.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
