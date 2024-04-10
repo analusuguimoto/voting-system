451af16a9dea48f7f544c16cb2253186e3220842
Ana Lourenco <analuizajl5@gmail.com>
2024-04-10 19:17:49 -0300

    implementing GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,12 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * implementing class GerenciamentoVotacao.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<String> cpfsComputados = new ArrayList<>();
 }
