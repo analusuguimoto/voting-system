755234bad60b404adcc5e0752b17673d1da929c1
Ana Lourenco <analuizajl5@gmail.com>
2024-04-10 18:08:28 -0300

    implementing class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,38 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
 
+/**
+ * implementing class PessoaCandidata.
+ */
+public class PessoaCandidata extends Pessoa {
+  /**
+   * methods and attributes of PessoaCandidata.
+   */
+  private int numero;
+  private int votos;
+
+  /**
+   * constructor PessoaCandidata.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void receberVoto() {
+    this.votos++;
+  }
 }
