51082453eb7445cca6afea09e9d9bc9d37eddfdd
Ana Lourenco <analuizajl5@gmail.com>
2024-04-10 16:59:24 -0300

    beginning and implementing abstract class Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * implementing class Pessoa.
+ */
+public abstract class Pessoa {
+  /**
+   * methods and attributes of Pessoa.
+   */
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
